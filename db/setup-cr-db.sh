78fd3e123cae76d6da3ea7d1caebed337c5697e4
Graham King <graham@gkgk.org>
2014-12-12 18:35:34 -0800

    Dockerfile tweaks

--- db/setup-cr-db.sh
+++ db/setup-cr-db.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "Creating database..."
 gosu postgres postgres --single <<- EOSQL
-   CREATE DATABASE carriagereturn;
-   CREATE USER postgres;
-   GRANT ALL PRIVILEGES ON DATABASE carriagereturn to postgres;
+	CREATE DATABASE carriagereturn WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+	ALTER DATABASE carriagereturn OWNER TO postgres;
 EOSQL
 echo "Created"
-echo "Importing db.sql..."
+echo "Running cr.sql..."
 gosu postgres postgres --single -j carriagereturn < /var/lib/cr.sql
-echo "Imported"
+echo "Done"
